d53c43bcc7a8124a365178d2cf17a764264b042e
Cristian Medina <medinacristian@hotmail.com>
2022-08-06 19:00:18 +0200

    Add CSS support (loaders)

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'none',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
