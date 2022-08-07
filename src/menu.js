7ce677d5b75984859a1c399d7763a0346df55410
Cristian Medina <medinacristian@hotmail.com>
2022-08-07 18:51:31 +0200

    Add three column to menu page

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,31 @@
 function loadMenu() {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const hdiv = document.createElement('div');
-    hdiv.classList.add('card-heading');
-    const heading = document.createElement('h2');
-    heading.textContent = 'Menu';
-    const bdiv = document.createElement('div');
-    bdiv.classList.add('card-content');
-    const lorem = document.createElement('p');
-    lorem.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus qui corporis at, excepturi cum unde aliquid ipsa ea amet quia natus reprehenderit iusto! Accusantium optio eligendi voluptate? Illo, quo nostrum.';
-    const links = document.createElement('div');
-    links.classList.add('card-content');
-    links.textContent = 'ABC';
+    const threeColumn = document.createElement('div');
+    threeColumn.classList.add('three-columns');
 
-    hdiv.appendChild(heading);
-    card.appendChild(hdiv);
-    bdiv.appendChild(lorem);
-    card.appendChild(bdiv);
-    card.appendChild(links);
+    for (let i = 1; i < 4; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const hdiv = document.createElement('div');
+        hdiv.classList.add('card-heading');
+        const heading = document.createElement('h2');
+        heading.textContent = 'Falafel ' + i;
+        const bdiv = document.createElement('div');
+        bdiv.classList.add('card-content');
+        const lorem = document.createElement('p');
+        lorem.textContent = 'Falafel, hummus, tahini, eggplant, tomato, red cabbage salad, mint, parsley';
+        const links = document.createElement('div');
+        links.classList.add('card-content');
+        links.textContent = '95 SEK';
+        
+        hdiv.appendChild(heading);
+        card.appendChild(hdiv);
+        bdiv.appendChild(lorem);
+        card.appendChild(bdiv);
+        card.appendChild(links);
+        threeColumn.appendChild(card);
+    }
 
-    return card
+    return threeColumn
 }
 
 export default loadMenu;
